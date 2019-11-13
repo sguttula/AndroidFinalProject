34c2bd38bd103c407bab57d880008ea24a7bcc23
mcmalfaro140 <mcmalfaro@hotmail.com>
2019-11-12 18:23:32 -0800

    everything is connected

--- app/src/main/java/com/example/myfinalproject/LogInFragment.kt
+++ app/src/main/java/com/example/myfinalproject/LogInFragment.kt
@@ -1,23 +1,64 @@
 package com.example.myfinalproject
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.myfinalproject.databinding.FragmentLogInBinding
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class LogInFragment : Fragment() {
+
+    private  lateinit var binding: FragmentLogInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_log_in, container, false )
+        binding.progressBar.setVisibility(View.GONE)
+
+        binding.login.setOnClickListener {
+            if (binding.switch1.isChecked){
+                val intent = Intent (getActivity(), MainActivity::class.java)
+                getActivity()?.startActivity(intent)
+            }else{
+                val intent = Intent (getActivity(), ProvidersMainActivity::class.java)
+                getActivity()?.startActivity(intent)
+            }
+        }
+
+        binding.signupbtn.setOnClickListener {view: View ->
+            if (binding.switch1.isChecked){
+                view.findNavController().navigate(R.id.action_logInFragment_to_singUpFragment)
+            }else{
+                view.findNavController().navigate(R.id.action_logInFragment_to_singUpFragment)
+            }
+        }
+
+        binding.forgotPass.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_logInFragment_to_forgotPasswordFragment)
+        }
+
+        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.switch1.text = "switch to Provider"
+                binding.mainText.setText(R.string.user)
+//                mainImg.setImageResource(R.drawable.user)
+            } else {
+                binding.switch1.text = "Switch to User"
+                binding.mainText.setText(R.string.provider)
+//                mainImg.setImageResource(R.drawable.provider)
+            }
+        }
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/myfinalproject/SplashActivity.kt
+++ app/src/main/java/com/example/myfinalproject/SplashActivity.kt
@@ -1,12 +1,29 @@
 package com.example.myfinalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.splash_background)
+
+        //2second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this, AuthenticationActivity::class.java))
+            //finish this activity
+            finish()
+        },2000)
+
     }
 }
